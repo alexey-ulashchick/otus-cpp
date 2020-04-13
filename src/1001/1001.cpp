da96f03717aad25e0bfc600b410b43dc41aeb404
Alexey Ulashchick <alexey@Alexeys-MacBook-Pro.local>
2020-04-12 17:55:31 -0700

    [HW2]: Task 1001

--- src/1001/1001.cpp
+++ src/1001/1001.cpp
@@ -1,24 +1,18 @@
 #include <iostream>
-#include <sstream>
-#include <vector>
 #include <cmath>
+#include <stack>
 
 int main(int, char **) {
-  std::stringstream stringStream;
-  std::string str;
+  std::stack<uint64_t> nums;
+  uint64_t value;
 
-  while (std::getline(std::cin, str)) {
-    stringStream << str << " ";
-  }
+  while (std::cin >> value) {
+		nums.push(value);
+	}
 
-  std::vector<long> nums;
-
-  while (stringStream >> str) {
-    nums.push_back(std::stol(str));
-  }
-
-  for (auto rit = nums.rbegin(); rit != nums.rend(); ++rit) {
-    printf("%.4f\n", std::sqrt(*rit));
+  while (!nums.empty()) {
+    printf("%.4f\n", std::sqrt(nums.top()));
+    nums.pop();
   }
 
   return 0;
